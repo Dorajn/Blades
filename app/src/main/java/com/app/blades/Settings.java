0be13df634bbfeb7236413acd0f07124dc5a07e0
Dorajn <derquv@gmail.com>
2024-10-09 16:39:41 +0200

    more settings develop

--- app/src/main/java/com/app/blades/Settings.java
+++ app/src/main/java/com/app/blades/Settings.java
@@ -1,12 +1,18 @@
 package com.app.blades;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +20,7 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -33,10 +40,15 @@ import com.google.firebase.firestore.Source;
 
 public class Settings extends AppCompatActivity {
 
+    //Settings
+    SharedPreferences sharedPref;
+
     EditText fuelPriceEditText;
     TextView infoCurrentFuelPrice;
 
-    Button goBack, apply;
+    ImageView applyFuelPrice;
+    Button goBack;
+    Switch lazyCalculations, lazyExtraCalculations;
 
     FirebaseAuth auth;
     FirebaseFirestore db;
@@ -48,14 +60,11 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        fuelPriceEditText = findViewById(R.id.editTextFuelPrice);
-        infoCurrentFuelPrice = findViewById(R.id.fuelPriceInfo);
-        goBack = findViewById(R.id.goBackSettings);
-        apply = findViewById(R.id.applyButton2);
+        sharedPref = Settings.this.getSharedPreferences("userSettings", MODE_PRIVATE);
 
-        auth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
-        userID = auth.getCurrentUser().getUid();
+        initializeVariables();
+        initializeGoogleVariables();
+        createSharedPrefFileIfNotExists();
 
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,7 +75,7 @@ public class Settings extends AppCompatActivity {
             }
         });
 
-        apply.setOnClickListener(new View.OnClickListener() {
+        applyFuelPrice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -106,12 +115,68 @@ public class Settings extends AppCompatActivity {
                 }
             }
         });
+
+        lazyCalculations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean("lazyFuelCalc", lazyCalculations.isChecked());
+                editor.apply();
+            }
+        });
+
+        lazyExtraCalculations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean("lazyExtraFuelCalc", lazyExtraCalculations.isChecked());
+                editor.apply();
+            }
+        });
+
+    }
+
+    private void createSharedPrefFileIfNotExists() {
+        if(!sharedPrefExists()){
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putBoolean("lazyFuelCalc", true);
+            editor.putBoolean("lazyExtraFuelCalc", false);
+            editor.apply();
+        }
+    }
+
+    private boolean sharedPrefExists() {
+        return sharedPref.contains("lazyFuelCalc");
+    }
+
+    private void initializeGoogleVariables() {
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        userID = auth.getCurrentUser().getUid();
+    }
+
+    private void initializeVariables() {
+        fuelPriceEditText = findViewById(R.id.editTextFuelPrice);
+        infoCurrentFuelPrice = findViewById(R.id.fuelPriceInfo);
+        goBack = findViewById(R.id.goBackSettings);
+        applyFuelPrice = findViewById(R.id.applyFuelPrice);
+        lazyCalculations = findViewById(R.id.lazySwitch);
+        lazyExtraCalculations = findViewById(R.id.lazyExtraSwitch);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        getFuelPriceFromDB();
+        setSwitches();
+    }
 
+    private void setSwitches() {
+        lazyCalculations.setChecked(sharedPref.getBoolean("lazyFuelCalc", true));
+        lazyExtraCalculations.setChecked(sharedPref.getBoolean("lazyExtraFuelCalc", false));
+    }
+
+    private void getFuelPriceFromDB() {
         userID = auth.getCurrentUser().getUid();
         db.collection("users")
                 .document(userID)
